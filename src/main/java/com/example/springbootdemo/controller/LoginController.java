7e8e68132a4daba1f8ab94dd0f347e04f898f81d
杨永涛 <3038622403@qq.com>
2019-01-22 10:13:49 +0800

    测试连接mongo副本集集群

--- src/main/java/com/example/springbootdemo/controller/LoginController.java
+++ src/main/java/com/example/springbootdemo/controller/LoginController.java
@@ -2,14 +2,13 @@ package com.example.springbootdemo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 @RequestMapping("/demo/login")
 public class LoginController {
 
     @RequestMapping("")
-    public String login(String userName,String passWord){
+    public String login(   String userName,String passWord){
         return "";
     }
 }
